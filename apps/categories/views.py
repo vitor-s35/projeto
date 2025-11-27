39559e0f0b6fe616d0d9b91ee027962abc82ca19
vitin <vitor.moraesti@gmail.com>
2025-11-26 23:39:19 -0300

    tirando token

--- apps/categories/views.py
+++ apps/categories/views.py
@@ -1,11 +1,11 @@
 from rest_framework import viewsets
 from .models import Category
 from .serializer import CategorySerializer
-from rest_framework.authentication import TokenAuthentication
+#from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer 
-    authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]
+    #authentication_classes = [TokenAuthentication]
+    #permission_classes = [IsAuthenticated]
